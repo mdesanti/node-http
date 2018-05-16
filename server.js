86083a8839c8ba96d79602b72ef7a6f815370165
Matias De Santi <matias.desanti@wolox.com.ar>
2018-05-16 12:29:17 -0500

    Testing PSQL

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var http = require('http');
 var fileSystem = require('fs');
 
 var server = http.createServer(function(req, resp){
+	var pg = require('pg');
+	var username = process.env.DB_USERNAME
+	var password = process.env.DB_PASSWORD
+	var host = process.env.DB_HOST
+	var port = process.env.DB_PORT
+	var databaseName = process.env.DB_NAME
+	var conString = `postgres://${username}:${password}@${host}:${port}/${databaseName}`;
+
+	var client = new pg.Client(conString);
+	client.connect();
+
 	fileSystem.readFile('./index.html', function(error, fileContent){
 		if(error){
 			resp.writeHead(500, {'Content-Type': 'text/plain'});
@@ -11,6 +22,7 @@ var server = http.createServer(function(req, resp){
 		else{
 			resp.writeHead(200, {'Content-Type': 'text/html'});
 			fileContent = fileContent.toString().replace('#variable', process.env.TEST_VARIABLE)
+			fileContent = fileContent.toString().replace('#connString', conString)
 			resp.write(fileContent);
 			resp.end();
 		}
